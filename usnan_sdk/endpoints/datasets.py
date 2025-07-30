dbf15c25344fb2b05dd6819b04813628c22e7874
Jon Wedell <jon@jonwedell.com>
2025-07-30 11:45:47 -0500

    Start fetching the next batch of search results immediately when the current batch is used up. Will rarely make a difference in speed, but will lead to a reasonable speed up when it triggers.

--- usnan_sdk/endpoints/datasets.py
+++ usnan_sdk/endpoints/datasets.py
@@ -1,5 +1,8 @@
 """Datasets endpoint implementation"""
 from typing import Generator
+import asyncio
+import concurrent.futures
+from threading import Thread
 
 from .base import BaseEndpoint
 from ..models.dataset import Dataset
@@ -21,22 +24,49 @@ class DatasetsEndpoint(BaseEndpoint):
         """
 
         config_copy: SearchConfig = search_config.clone()
+        next_batch_future = None
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
 
-        while True:
-            response = self._get('/nan/public/datasets/search', params=config_copy.build())
-            for item in response.get('experiments', []):
-                yield Dataset.from_dict(self.client, item)
-            if response.get('last_page'):
-                return
-            config_copy.offset += config_copy.records
+        def fetch_batch(config):
+            """Helper function to fetch a batch of data"""
+            return self._get('/nan/public/datasets/search', params=config.build())
 
-            # Double the amount of records fetched at a time each time they are exhausted, but don't
-            #  fetch more than 1000 at a time.
-            if config_copy.records < 1000:
-                new_records = int(config_copy.records * 2)
-                config_copy.records = new_records
-                if new_records > 1000:
-                    config_copy.new_records = 1000
+        try:
+            while True:
+                # Get current batch (either first request or from prefetched future)
+                if next_batch_future is None:
+                    response = fetch_batch(config_copy)
+                else:
+                    response = next_batch_future.result()
+                
+                # Prepare next batch config before yielding current results
+                next_config = config_copy.clone()
+                next_config.offset += next_config.records
+                
+                # Double the amount of records fetched at a time each time they are exhausted, but don't
+                #  fetch more than 1000 at a time.
+                if next_config.records < 1000:
+                    new_records = int(next_config.records * 2)
+                    next_config.records = new_records
+                    if new_records > 1000:
+                        next_config.records = 1000
+
+                # Start prefetching next batch if not on last page
+                if not response.get('last_page'):
+                    next_batch_future = executor.submit(fetch_batch, next_config)
+                else:
+                    next_batch_future = None
+
+                # Yield current batch results
+                for item in response.get('experiments', []):
+                    yield Dataset.from_dict(self.client, item)
+                
+                if response.get('last_page'):
+                    return
+                
+                config_copy = next_config
+        finally:
+            executor.shutdown(wait=False)
     
     def get(self, dataset_id: str) -> Dataset:
         """
