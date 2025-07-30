904345fb984c04b2d879265782290b4637df0fff
Jon Wedell <jon@jonwedell.com>
2025-07-30 11:39:23 -0500

    Add the SearchConfig option to the models __init__

--- usnan_sdk/models/__init__.py
+++ usnan_sdk/models/__init__.py
@@ -2,5 +2,6 @@ from .probes import Probe, SensitivityMeasurement
 from .facilities import Facility
 from .dataset import Dataset
 from .spectrometers import Spectrometer
+from .search import SearchConfig
 
 __all__ = ["Dataset", "Facility", "Spectrometer", "Probe", "SensitivityMeasurement"]
