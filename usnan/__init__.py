fece9717854c82662e31e7507ad07758534b7631
Gerard Weatherby <gweatherby@uchc.edu>
2025-09-11 10:01:26 -0400

    Read version from pyproject.toml metadata

--- usnan/__init__.py
+++ usnan/__init__.py
@@ -1,7 +1,9 @@
 """USNAN SDK - Python SDK for USNAN API"""
 
+import importlib.metadata 
 from .client import USNANClient
 from . import models
 
-__version__ = "0.1.0"
+__version__ =  importlib.metadata.version('usnan') 
+
 __all__ = ["USNANClient", "models"]
